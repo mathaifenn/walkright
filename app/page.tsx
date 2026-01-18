c32459195eda106d6c7ee15002494e6985e80398
Mathai Fenn <mathaifenn@gmail.com>
2026-01-18 07:38:28 +0530

    Redirect root to reflection page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main>
-      <h1>WalkRight</h1>
-      <p>Local dev working.</p>
-    </main>
-  );
+  redirect("/index.html");
 }
